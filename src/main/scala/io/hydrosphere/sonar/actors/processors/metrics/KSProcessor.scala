712d9302e863f2f3221d8c733d1074a8ea140c34
Vadim Chelyshov <qtankle@gmail.com>
2019-01-31 13:20:49 +0300

    add traceData to metric labels

--- src/main/scala/io/hydrosphere/sonar/actors/processors/metrics/KSProcessor.scala
+++ src/main/scala/io/hydrosphere/sonar/actors/processors/metrics/KSProcessor.scala
@@ -42,7 +42,13 @@ object KSProcessor {
     val ksFn = (sample: NonEmptyList[Double]) => KolmogorovSmirnovTest.test(sample, Statistics.generateDistribution(Statistics.Distribution.Normal, 100))
     val transposed = CollectionOps.safeTranspose(flatRequests)
     transposed.filter(_.nonEmpty).map(l => NonEmptyList(l.head, l.toList.tail)).map(ksFn).zipWithIndex.flatMap { case (ksResult, idx) =>
-      val labels = Map("modelVersionId" -> metricSpec.modelVersionId.toString, "columnIndex" -> idx.toString)
+      
+      val labels = Map(
+        "modelVersionId" -> metricSpec.modelVersionId.toString,
+        "columnIndex" -> idx.toString,
+        "traces" -> Traces.many(requests.map(_.payload))
+      )
+      
       val health = if (metricSpec.withHealth) {
         Some(ksResult.value <= ksResult.rejectionLevel)
       } else None
