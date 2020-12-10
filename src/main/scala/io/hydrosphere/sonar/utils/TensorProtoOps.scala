6e6f19d3d5d391cd1bca5a76e86ce3c66c79cba2
Ilnur Garifullin <ilnur.email@gmail.com>
2020-12-10 17:41:53 +0300

    Fix tensor value conversions

--- src/main/scala/io/hydrosphere/sonar/utils/TensorProtoOps.scala
+++ src/main/scala/io/hydrosphere/sonar/utils/TensorProtoOps.scala
@@ -7,21 +7,20 @@ object TensorProtoOps {
   
   implicit class TensorProtoConversions(tensorProto: TensorProto) {
     
-    def toDoubles: Seq[Double] = TypedTensorFactory.create(tensorProto) match {
-      case DoubleTensor(_, data) => data
-      case FloatTensor(_, data) => data.map(_.toDouble)
+    def toDoubles: Option[Seq[Double]] = TypedTensorFactory.create(tensorProto) match {
+      case DoubleTensor(_, data) => Some(data)
+      case FloatTensor(_, data) => Some(data.map(_.toDouble))
       case integers @ (_: Uint64Tensor | _: Int64Tensor) =>
-        integers.data.asInstanceOf[Seq[Long]].map(_.toDouble)
+        Some(integers.data.asInstanceOf[Seq[Long]].map(_.toDouble))
       case integers@(_: Int32Tensor | _: Uint32Tensor | _: Uint8Tensor | _: Int8Tensor | _: Int16Tensor) =>
-        integers.data.asInstanceOf[Seq[Int]].map(_.toDouble)
-      case BoolTensor(_, data) => data.map(x => if (x) 1.0 else 0.0)
-
-      case unknownTensorType => throw new Exception(s"$unknownTensorType cannot be cast to Seq[Double]")
+        Some(integers.data.asInstanceOf[Seq[Int]].map(_.toDouble))
+      case BoolTensor(_, data) => Some(data.map(x => if (x) 1.0 else 0.0))
+      case _ => None
     }
     
-    def toStrings: Seq[String] = TypedTensorFactory.create(tensorProto) match {
-      case StringTensor(_, data) => data
-      case unknownTensorType => throw new Exception(s"$unknownTensorType cannot be cast to Seq[String]")
+    def toStrings: Option[Seq[String]] = TypedTensorFactory.create(tensorProto) match {
+      case StringTensor(_, data) => Some(data)
+      case _ => None
     }
     
   }
